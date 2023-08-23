c176d26ffcb27a00048cdb0cd137ca4ff39d5c41
BakytDzhunushov <bahanur2023@gmail.com>
2023-08-23 15:49:12 +0600

    igra v kosti_kubik

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+//import 'package:sabak_f15/home_page.dart';
+import 'home2page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Home2Page(),
+    );
+  }
+}
